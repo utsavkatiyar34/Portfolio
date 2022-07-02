78c98cf4d8f476a1dc4336d48e3c60fb892e35e9
utsavkatiyar34 <utsavkatiyar34@gmail.com>
2022-07-02 11:55:54 +0530

    education done

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Project from './components/Projects';
-import Skills from './components/Skills';
-import Education from './components/Education';
-import {BrowserRouter} from "react-router-dom";
-import{Routes}from "react-router-dom";
-import{Route}from"react-router-dom";
-import Home from './components/Home';
-import ContactMe from './components/ContactMe';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Project from "./components/Projects";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import ContactMe from "./components/ContactMe";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
-    <App />
-    <Routes>
-      <Route exact path="/" element={<Home/>}>
-      </Route>
-      <Route exact path="/skills" element={<Skills/>}>
-      </Route>
-      <Route exact path="/projects" element={<Project/>}>
-      </Route>
-      <Route exact path="/education" element={<Education/>}>
-      </Route>
-      <Route exact path="/contact" element={<ContactMe/>}>
-      </Route>
+    <React.StrictMode>
+      <App />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/skills" element={<Skills />}></Route>
+        <Route exact path="/projects" element={<Project />}></Route>
+        <Route exact path="/education" element={<Education />}></Route>
+        <Route exact path="/contact" element={<ContactMe />}></Route>
       </Routes>
-  </React.StrictMode>
+    </React.StrictMode>
   </BrowserRouter>
 );
 
